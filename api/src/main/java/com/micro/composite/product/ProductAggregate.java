d11c3ecbdafd21934ff972012d7f94796c790264
Suman Gajmer <suman.gajmer@f1soft.com>
2024-02-24 11:16:08 +0545

    chapter 03 completed

---
+++ api/src/main/java/com/micro/composite/product/ProductAggregate.java
@@ -0,0 +1,8 @@
+package com.micro.composite.product;
+
+
+import java.util.List;
+
+public record ProductAggregate(int productId, String name, int weight, List<RecommendationSummary> recommendations,
+                               List<ReviewSummary> reviews, ServiceAddresses serviceAddresses) {
+}
---
+++ api/src/main/java/com/micro/composite/product/ReviewSummary.java
@@ -0,0 +1,6 @@
+package com.micro.composite.product;
+
+
+public record ReviewSummary(int reviewId, String author, String subject) {
+
+}
